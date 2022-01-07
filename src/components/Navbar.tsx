ac597d1b37ea8e1dd205f5cfa0fbf15c3929795f
Amit-x360 <amit.amit.kumar761@gmail.com>
2022-01-07 12:31:25 +0530

    fetch currently logged user & graphql cache update

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import React from "react";
+import { ReactElement } from "react";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body: ReactElement | null = null;
+
+  // data is loading
+  if (fetching) {
+    body = <Box>Loading...</Box>;
+  }
+  // user is not logged in
+  else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link color="white" mr={2}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white">Register</Link>
+        </NextLink>
+      </>
+    );
+  }
+  // user is logged in
+  else {
+    body = (
+      <Flex>
+        <Box mr={5}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml="auto">{body}</Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
